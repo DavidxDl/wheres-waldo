639b438bec7493b3aa3bd74e30b7a5cc1eb80da7
David D <Davidbekamlopez@gmail.com>
2024-02-26 16:05:52 -0300

    -added a sign to the CharacterList Component when you guess correctly and one when u dont

--- src/app/components/CharacterList.tsx
+++ src/app/components/CharacterList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   onTarget: React.MutableRefObject<true | false>;
@@ -6,19 +6,31 @@ interface Props {
 }
 
 const CharacterList = ({ onTarget, position }: Props) => {
+  const [result, setResults] = useState<null | string>(null);
+  function handleClick() {
+    if (onTarget.current) {
+      setResults("✅");
+    } else {
+      setResults("❌");
+    }
+  }
   return (
     <div
       style={{ left: position.x, top: position.y }}
-      className="z-10 rounded p-3 bg-white/30 border-[1px] border-white/80 backdrop-blur-[2px] absolute h-32 w-28"
+      className="flex justify-center z-10 rounded p-3 bg-white/30 border-[1px] border-white/80 backdrop-blur-[2px] absolute h-32 w-28"
     >
-      <ul className="flex flex-col items-center w-full">
-        <li
-          onClick={() => onTarget.current && console.log("YOU WIN!")}
-          className="transition-transform duration-400 active:translate-y-[-10px] hover:scale-110 hover:cursor-pointer p-0 m-0"
-        >
-          👲 Waldo
-        </li>
-      </ul>
+      {result ? (
+        <p className="hover:cursor-default">{result}</p>
+      ) : (
+        <ul className="flex flex-col items-center w-full">
+          <li
+            onClick={handleClick}
+            className="transition-transform duration-400 active:translate-y-[-10px] hover:scale-110 hover:cursor-pointer p-0 m-0"
+          >
+            👲 Waldo
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
