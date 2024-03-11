11277899a250ee30f99b86767a18a4d9437f459a
David D <Davidbekamlopez@gmail.com>
2024-03-11 02:25:01 -0300

    working on the ui of the game

--- src/app/images/[imageId]/page.tsx
+++ src/app/images/[imageId]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { character } from "~/app/testing/page";
 import ImageGame from "~/components/ImageGame";
 
 export default async function ImagePage({
@@ -10,10 +11,21 @@ export default async function ImagePage({
   return (
     <div>
       <ImageGame image={image} />
-      <div className="flex flex-col">
-        <h1 className="text-center text-3xl font-extrabold text-white">
-          Some Info
-        </h1>
+      <div className="flex flex-col bg-black/90 backdrop-blur-md text-white">
+        <div className="flex px-5  text-3xl font-extrabold text-white">
+          <div>
+            <p>Characters: </p>
+          </div>
+          <div className="text-center flex-grow ">Clock</div>
+        </div>
+        <div className="md:hidden">
+          <h2 className="text-2xl font-extrabold text-center">Characters:</h2>
+          <ul className="align-center gap-4 justify-center flex text-white font-extrabold text-2xl w-full">
+            {image.characters.map((char: character) => (
+              <li>{char.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
