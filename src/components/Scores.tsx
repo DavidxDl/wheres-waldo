e17ad452b8ad5c9181213279ff921c965ffbef17
David D <Davidbekamlopez@gmail.com>
2024-03-15 03:53:29 -0300

    aperently the type html atribute is not sopported so i used a tailwind class instead

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -9,8 +9,8 @@ export default function Scores({ scores }: Props) {
   return (
     <aside className="flex flex-col items-center  md:min-w-40 bg-red-500/50">
       <h1 className="font-extrabold text-white text-center">SCORES</h1>
-      <ol className="mt-5 text-white font-bold">
-        {scores.sort((a, b) => a.score - b.score).map(s => <li type="1">{s.name}: {s.score}</li>)}
+      <ol className="mt-5 text-white font-bold list-decimal">
+        {scores.sort((a, b) => a.score - b.score).map((s, index) => <li key={index}>{s.name}: {s.score}</li>)}
       </ol>
     </aside>
   )
