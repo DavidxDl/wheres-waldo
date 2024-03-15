7489b7155cd4615318d58fb97bba688fb0107c35
David D <Davidbekamlopez@gmail.com>
2024-03-15 05:17:03 -0300

    remove a component

--- src/lib/mongoDB.ts
+++ src/lib/mongoDB.ts
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-import { NextApiRequest, NextApiResponse } from "next";
 
 async function connectMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URL || "");
+    await mongoose.connect(process.env.MONGO_URL ?? "");
     console.log("connected to MONGODB");
   } catch (error) {
     console.log("couldnt connect to MONGODB");
