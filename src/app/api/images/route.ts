39b17f0e5b699e135f6ab9505063dadb59a691cd
David D <Davidbekamlopez@gmail.com>
2024-03-15 02:41:55 -0300

    fix some typos in the api

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,10 +1,8 @@
 import connectMongoDB from "~/lib/mongoDB";
 import image from "models/image";
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
-export async function GET(req: NextApiRequest) {
+export async function GET(req: Response) {
   await connectMongoDB();
   const images = await image.find({});
   console.log(images);
-  return NextResponse.json(images);
+  return Response.json(images);
 }
