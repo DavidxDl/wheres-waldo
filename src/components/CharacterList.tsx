ba5628963e61f77c0f300f57a97cfe4a7fa76a7f
David D <Davidbekamlopez@gmail.com>
2024-03-16 20:55:11 -0300

    -added some more style to the character list

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -34,22 +34,18 @@ const CharacterList = ({
         left: mousePos.x,
         top: mousePos.y,
       }}
-      className="text-shadow roll-down text-2xl text-[#ff0]  shadow shadow-black flex justify-center z-10 rounded p-3 bg-white/30 border-[1px] border-white/80 backdrop-blur-[2px] absolute min-h-32 min-w-28"
+      className="text-shadow roll-down text-2xl text-[#ff0]  shadow shadow-black z-10 rounded p-3 bg-white/30 border-[1px] border-white/80 backdrop-blur-[2px] absolute min-h-32 min-w-28"
     >
-      <ul className="flex flex-col items-center w-full">
+      <ul className="flex flex-col items-start gap-2 w-full">
         {characters.map(
           (char) =>
             !discoveredCharacters.has(char.name) && (
               <li
                 key={char.name}
                 onClick={() => handleClick(char)}
-                className="
-            capitalize font-bold
-            hover:scale-150 hover:cursor-pointer
-            active:translate-y-[-2px] transition-transform
-
-            "
+                className="w-full flex gap-2 items-center capitalize font-bold hover:scale-125 hover:cursor-pointer active:translate-y-[-2px] transition-transform "
               >
+                <img className="w-10 rounded-full" src={`/images/${char.name}.jpg`} />
                 {char.name}
               </li>
             )
@@ -64,13 +60,13 @@ function getMousePos(imageSize: DOMRect, position: { x: number; y: number }) {
     x: position.x * imageSize.width,
     y: position.y * imageSize.height,
   };
-  if (tmpPos.x + OFFSET * 3 >= imageSize.width) {
-    tmpPos.x -= OFFSET * 4;
+  if (tmpPos.x + OFFSET * 5 >= imageSize.width) {
+    tmpPos.x -= OFFSET * 5;
   } else {
     tmpPos.x += OFFSET;
   }
-  if (tmpPos.y + OFFSET * 2 >= imageSize.height) {
-    tmpPos.y -= OFFSET * 3;
+  if (tmpPos.y + OFFSET * 4 >= imageSize.height) {
+    tmpPos.y -= OFFSET * 5;
   }
   return tmpPos;
 }
