f1c3c9f01fb2505feabe10416e2a33b05c34dd37
David D <Davidbekamlopez@gmail.com>
2024-03-16 18:00:10 -0300

    -change character 'Danilo' to 'wenda' change all the images to jpeg ones

--- src/components/CharacterListItem.tsx
+++ src/components/CharacterListItem.tsx
@@ -9,7 +9,7 @@ function CharacterListItem({ isFound, character }: Props) {
   return (
     <li className={` flex flex-col items-center justify-center  `}>
       <div className={`${isFound ? "found-character" : ""}`}>
-        <img className="w-14 h-14 rounded-full" src={`/images/${character}.webp`} />
+        <img className="w-14 h-14 rounded-full" src={`/images/${character}.jpg`} />
       </div >
       {character}
     </li>
